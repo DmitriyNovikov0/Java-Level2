488fa106e957cb992598ce46be6202031d1434d5
Dmitriy <adin@mail.ru>
2019-07-28 20:09:26 +0600

    start wh

---
+++ chat/ChatServer.java
@@ -0,0 +1,11 @@
+package chat;
+
+public class ChatServer {
+    void start(int port) {
+        System.out.println("Server started at port: " + port);
+    }
+
+    void stop() {
+        System.out.println("Server stopped");
+    }
+}
